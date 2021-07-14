bf6e8a949d179886aa38ff62c70d332d0f44e4bf
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-07-14 10:49:08 +0200

    added user model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+interface UserAttributes {
+  email: string;
+  password: string;
+}
+
+interface UserDocument extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDocument> {
+  build(attributes: UserAttributes): UserDocument;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.statics.build = (attributes: UserAttributes) => {
+  return new User(attributes);
+};
+
+export const User = mongoose.model<UserDocument, UserModel>('User', userSchema);
