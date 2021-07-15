3bcafb11155b3648687b385f584aae9001fd7492
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-07-15 06:56:57 +0200

    refactor

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import 'express-async-errors';
+
+import cookieSession from 'cookie-session';
+
+import { NotFoundError } from './errors';
+import { errorHandler } from './middlewares';
+import {
+  currentUserRouter,
+  signinRouter,
+  signupRouter,
+  signoutRouter,
+} from './routes';
+
+export const app = express();
+app.set('trust proxy', true);
+
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
