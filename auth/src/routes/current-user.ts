c96d841eff6ef307b0f5b5d5dc1582fd0862cbec
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-07-16 11:45:20 +0200

    current user testing

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -5,7 +5,7 @@ import { currentUser } from '../middlewares';
 const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
+  res.status(200).send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
