64bc90be3d1da745b42fe8fba7ac6ef20f16d588
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-07-31 20:15:57 +0200

    added signin page

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import Router from 'next/router';
+
+import { useRequest } from '../../hooks';
+
+const Signin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signin',
+    method: 'post',
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push('/'),
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    await doRequest();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Sign In</h1>
+      <div className='form-group mt-3'>
+        <label>Email Adress</label>
+        <input
+          className='form-control'
+          type='text'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className='form-group mt-3'>
+        <label>Password</label>
+        <input
+          className='form-control'
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {errors}
+      <button className='btn btn-primary mt-3'>Sign In</button>
+    </form>
+  );
+};
+
+export default Signin;
