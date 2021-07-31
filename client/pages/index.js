842a92f91c7dfc216b671229b5e6250a6f102cfa
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-07-31 19:53:56 +0200

    fetching data during SSR

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,25 @@
-const Landing = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
 };
 
-export default Landing;
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+};
+
+export default LandingPage;
