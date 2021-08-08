7866aa29d2b37d479cab8944f82ee89e0f957f7b
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-08-08 16:33:08 +0200

    added create ticket functionality

--- tickets/src/routes/create-ticket.ts
+++ tickets/src/routes/create-ticket.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 
 import { requireAuth, validateRequest } from '@zhtickets/common';
+import { Ticket } from '../models';
 
 const router = express.Router();
 
@@ -15,8 +16,17 @@ router.post(
       .withMessage('Price must be greater then 0'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+    await ticket.save();
+
+    res.status(201).send(ticket);
   }
 );
 
