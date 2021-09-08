b69f79b535b06f3942633058858c217c33edab86
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-09-08 21:52:04 +0200

    added ticket created publisher

--- tickets/src/routes/create-ticket.ts
+++ tickets/src/routes/create-ticket.ts
@@ -3,6 +3,7 @@ import { body } from 'express-validator';
 
 import { requireAuth, validateRequest } from '@zhtickets/common';
 import { Ticket } from '../models';
+import { TicketCreatedPublisher } from '../events';
 
 const router = express.Router();
 
@@ -25,6 +26,12 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
+    await new TicketCreatedPublisher(client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }
