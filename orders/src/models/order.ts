f52bb6474a3fd606201d7f40f97bdaee7070ff4b
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-09-19 00:35:21 +0200

    added order model

---
+++ orders/src/models/order.ts
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+
+interface OrderAttributes {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDocument;
+}
+
+interface OrderDocument extends mongoose.Document {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDocument;
+}
+
+interface OrderModel extends mongoose.Model<OrderDocument> {
+  build(attributes: OrderAttributes): OrderDocument;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      require: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attributes: OrderAttributes) => {
+  return new Order(attributes);
+};
+
+const Order = mongoose.model<OrderDocument, OrderModel>('Order', orderSchema);
+
+export { Order };
