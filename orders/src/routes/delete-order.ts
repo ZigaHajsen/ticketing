ddb5b0ef2c2791aae213359a98d32bad0e02cd9c
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-09-18 21:04:42 +0200

    changed name

--- orders/src/routes/delete-order.ts
+++ orders/src/routes/delete-order.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (re: Request, res: Response) => {
+router.delete('/api/orders/:id', async (re: Request, res: Response) => {
   res.send({});
 });
 
