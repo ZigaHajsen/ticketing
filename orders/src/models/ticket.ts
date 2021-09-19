4bb8862f2c221a8dfb750e19ee517eb0db007337
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-09-19 08:10:16 +0200

    added new order logic

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+import { Order, OrderStatus } from './order';
+
 interface TicketAttributes {
   title: string;
   price: number;
@@ -8,13 +10,14 @@ interface TicketAttributes {
 export interface TicketDocument extends mongoose.Document {
   title: string;
   price: number;
+  isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDocument> {
   build(attributes: TicketAttributes): TicketDocument;
 }
 
-const ticketSchema = new mongoose.Schema(
+const ticketSchema = new mongoose.Schema<TicketDocument>(
   {
     title: {
       type: String,
@@ -38,6 +41,21 @@ const ticketSchema = new mongoose.Schema(
 
 ticketSchema.statics.build = (attributes: TicketAttributes) => {
   return new Ticket(attributes);
+};
+
+ticketSchema.methods.isReserved = async function () {
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.Complete,
+        OrderStatus.AwaitingPayment,
+      ],
+    },
+  });
+
+  return !!existingOrder;
 };
 
 const Ticket = mongoose.model<TicketDocument, TicketModel>(
