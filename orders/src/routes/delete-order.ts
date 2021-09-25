7058af1c482fc2390d0923cf4c43226204121a91
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-09-25 17:42:27 +0200

    added delete order route

--- orders/src/routes/delete-order.ts
+++ orders/src/routes/delete-order.ts
@@ -1,9 +1,35 @@
 import express, { Request, Response } from 'express';
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+} from '@zhtickets/common';
+
+import { Order } from '../models';
 
 const router = express.Router();
 
-router.delete('/api/orders/:id', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  '/api/orders/:id',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    const order = await Order.findById(id);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
