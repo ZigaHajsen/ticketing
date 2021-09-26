c5e6bb173564a7ba557be11839d573233a3c8216
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-09-26 08:24:56 +0200

    added tests

--- orders/src/routes/__test__/new-order.test.ts
+++ orders/src/routes/__test__/new-order.test.ts
@@ -3,6 +3,7 @@ import request from 'supertest';
 
 import { app } from '../../app';
 import { Order, OrderStatus, Ticket } from '../../models';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('return an error if the ticket does not exist', async () => {
   const ticketId = mongoose.Types.ObjectId();
@@ -41,4 +42,17 @@ it('reserves a ticket', async () => {
     .set('Cookie', global.signup())
     .send({ ticketId: ticket.id })
     .expect(201);
+});
+
+it('emits an order created event', async () => {
+  const ticket = Ticket.build({ title: 'concert', price: 20 });
+  await ticket.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signup())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
